6793c00d729f039517d3187a53dfcc03f9ed54c3
liuxingzhiwu <3170693374@qq.com>
2022-04-13 20:59:37 +0800

    1.0.5

--- src/main/java/com/liuxingzhiwu/test/MyTest.java
+++ src/main/java/com/liuxingzhiwu/test/MyTest.java
@@ -6,5 +6,8 @@ public class MyTest {
         System.out.println("hello,world");
         System.out.println("hello,liuxingzhiwu");
         System.out.println("hello,heihei");
+        System.out.println("hello,ppt");
+
+
     }
 }
